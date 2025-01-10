c144f536a4d2382281c44699d4c2461790c7e131
Jens Shumway <jensshum@gmail.com>
2025-01-10 16:10:15 -0700

    SECOND

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import PorterDrummer from "../src/assets/466604986_18464309254053626_5160230373881321912_n.jpg"
+import { Facebook, Instagram, Twitter, Youtube, Music } from 'lucide-react';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -27,40 +28,59 @@ function App() {
   return (
     <div className="min-h-screen bg-black flex flex-col items-center justify-center p-4">
       {/* Title */}
+      <img src="https://web.archive.org/web/20091026115428/http://geocities.com/cannabis_corner420/images/420clock.gif"></img>
+
       <h1 className="text-white text-4xl mb-12 tracking-widest">
         PORTER CHAPMAN
       </h1>
 
       {/* Vertical navigation menu */}
-      <nav className="bg-black bg-opacity-95 px-8 py-4">
+      <nav className="bg-black bg-opacity-95  py-10">
         <ul className="flex flex-col space-y-6">
           <li>
-            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-lg tracking-widest capitalize">
+            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-sm tracking-widest capitalize">
               Home
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-lg tracking-widest capitalize">
+            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-sm tracking-widest capitalize">
               About
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-lg tracking-widest capitalize">
+            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-sm tracking-widest capitalize">
               Collaborate
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-lg tracking-widest capitalize">
+            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-sm tracking-widest capitalize">
               Socials
             </a>
           </li>
           <li>
-            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-lg tracking-widest capitalize">
+            <a href="#" className="text-white hover:text-red-500 hover:translate-x-2 inline-block transition-all duration-200 text-sm tracking-widest capitalize">
               Gallery
             </a>
           </li>
         </ul>
       </nav>
+      <div className="flex space-x-9">
+        <a href="#" className="text-white hover:text-red-500 transition-colors duration-200">
+          <Facebook className="w-5 h-5" />
+        </a>
+        <a href="#" className="text-white hover:text-red-500 transition-colors duration-200">
+          <Instagram className="w-5 h-5" />
+        </a>
+        <a href="#" className="text-white hover:text-red-500 transition-colors duration-200">
+          <Twitter className="w-5 h-5" />
+        </a>
+        <a href="#" className="text-white hover:text-red-500 transition-colors duration-200">
+          <Youtube className="w-5 h-5" />
+        </a>
+        <a href="#" className="text-white hover:text-red-500 transition-colors duration-200">
+          <Music className="w-5 h-5" />
+        </a>
+      </div>
     </div>
   );
 }
